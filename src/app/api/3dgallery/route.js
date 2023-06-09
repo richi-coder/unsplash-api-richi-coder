b6b19b123e523d31968e4dcfda39971b23258579
richi-coder <richicoder@gmail.com>
2023-06-09 17:06:58 -0400

    deploying

--- src/app/api/3dgallery/route.js
+++ src/app/api/3dgallery/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
     const { searchParams } = new URL(request.url)
     const query = searchParams.get('query');
-    const res = await fetch(`https://api.unsplash.com/search/photos/?per_page=20&query=${query}&client_id=${process.env.VITE_TEST_KEY}`)    
+    const res = await fetch(`https://api.unsplash.com/search/photos/?per_page=20&query=${query}&client_id=${process.env.VITE_TEST_KEY}`)
     const data = await res.json();
     const results = data.results;
     return NextResponse.json(results);
